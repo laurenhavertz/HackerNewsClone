0dc9d81cbc04e4388ad5ba6749878f2749307801
Lauren Havertz <lauren.havertz@gmail.com>
2013-07-29 14:47:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerNews::Application.config.secret_token = '8c0df80046c45e33b5434d2298049dc62fa27c60f630306560bb81cec42ff9bd3284a3e0a6ce1c945c98cc4ea7b919e2305a3b5e199044c45ff4b4219664cec3'
