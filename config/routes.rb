1eaf2260bc8f9e36c4c7e761f560853634036b7b
Lauren Havertz <lauren.havertz@gmail.com>
2013-07-29 16:29:35 -0700

    greenlight

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 HackerNews::Application.routes.draw do
-  # The priority is based upon order of creation:
+
+  resources :links
+    # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
---
+++ spec/factories/active_record.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+
+  factory :link do
+    url 'www.google.com'
+    title 'title'
+  end
+
+end
